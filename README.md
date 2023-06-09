bc09644d0fa101deb06986c75de21e9a90f7fe3b
Joseph Oravetz <jjoravet@gmail.com>
2023-06-09 15:44:16 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# Alpaca-Snapshot-Analyzer
-The Alpaca Snapshot Analyzer is a Python script that fetches and analyzes real-time and historical stock snapshots using the Alpaca API, providing valuable insights for traders, investors, and analysts.
+# Alpaca Snapshot Analyzer
+
+The Alpaca Snapshot Analyzer is a Python script that fetches and analyzes stock snapshots using the Alpaca API. It provides real-time and historical data insights for multiple symbols, including trade, quote, minute bar, daily bar, and previous daily bar information.
+
+With the Alpaca Snapshot Analyzer, you can easily retrieve key data points for stocks and extract relevant information such as timestamps, prices, volumes, and more. The script offers flexibility in filtering data by type, allowing you to focus on specific aspects of the stock snapshots.
+
+Getting started is simple. You just need Python 3.x installed on your machine and valid Alpaca API credentials, including an API key ID and secret key ID. Optionally, you can provide a stock symbol list file to analyze specific stocks. Once set up, the script fetches the snapshots, processes the data, and presents the results in a structured format for further analysis or reporting.
+
+## Key Features
+
+- Fetch stock snapshots for multiple symbols simultaneously.
+- Extract trade, quote, minute bar, daily bar, and previous daily bar data.
+- Filter the data by type to focus on specific subsets.
+- Output the processed data in a structured format for easy analysis.
+
+The Alpaca Snapshot Analyzer is designed for traders, investors, and analysts who require detailed and up-to-date stock data. It provides valuable insights for making informed decisions, identifying trends, and performing data-driven analysis.
+
+Contributions to the Alpaca Snapshot Analyzer project are welcome. If you encounter any issues, have suggestions for improvements, or would like to contribute new features, please feel free to open an issue or submit a pull request.
+
+The project is open source and released under the MIT License, allowing you to modify, distribute, and use the code according to the license terms.
+
+Get started with the Alpaca Snapshot Analyzer today and gain valuable insights into stock snapshots to enhance your trading and investment strategies.
+
+## Prerequisites
+
+Before running the script, ensure you have the following:
+
+- Python 3.x installed on your machine.
+- Alpaca API credentials (API key ID and secret key ID).
+- Stock symbol list file (optional).
+
+## Installation
+
+1. Clone this repository to your local machine:
+
+```
+git clone https://github.com/JOravetz/alpaca-snapshot-analyzer.git
+```
+2. Install the required dependencies using pip:
+
+```
+pip install -r requirements.txt
+```
+
+3. Set up your Alpaca API credentials by exporting them as environment variables:
+
+```
+export APCA_API_KEY_ID=<your-api-key-id>
+export APCA_API_SECRET_KEY=<your-secret-key-id>
+export APCA_API_BASE_URL=<base-url>
+```
+
+## Usage
+
+1. Run the script with the desired options:
+
+```
+python snapshot_analyzer.py [-l <symbol-list-file>] [-t <data-type>]
+
+```
+
+- `<symbol-list-file>` (optional): Path to the file containing the stock symbols.
+- `<data-type>` (optional): Type of data to output (all, trade, quote, minute_bar, daily_bar, prev_daily_bar).
+
+If a stock symbol list file is not provided, the script will use active symbols with a symbol length less than 5.
+
+2. The script will fetch the snapshots, process the data, and output the results in a structured format.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
---
+++ alpaca_snapshot_analyzer.py
@@ -0,0 +1,252 @@
+import os
+import argparse
+import pandas as pd
+from alpaca_trade_api import REST
+from alpaca_trade_api.rest import APIError
+
+# Set pandas display options
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 200)
+
+# Load Alpaca API credentials from the environment variables
+API_KEY_ID = os.getenv("APCA_API_KEY_ID")
+SECRET_KEY_ID = os.getenv("APCA_API_SECRET_KEY")
+BASE_URL = os.getenv("APCA_API_BASE_URL")
+
+# Create REST API instance
+rest_api = REST(API_KEY_ID, SECRET_KEY_ID, BASE_URL)
+
+
+def fetch_snapshots(symbols):
+    """Fetch snapshots for given symbols.
+
+    Args:
+        symbols: List of symbols.
+
+    Returns:
+        A dictionary with symbols as keys and snapshot objects as values.
+    """
+    try:
+        snapshots = rest_api.get_snapshots(symbols)
+    except APIError as e:
+        print(f"Failed to fetch snapshots: {e}")
+        return {}
+    return snapshots
+
+
+def process_snapshot(symbol, snapshot):
+    """Process a snapshot to extract relevant data.
+
+    Args:
+        symbol: The symbol of the snapshot.
+        snapshot: The snapshot object.
+
+    Returns:
+        A dictionary with the processed data.
+    """
+    # Mapping for different data types within a snapshot
+    base_mapping = {
+        "t": "timestamp",
+        "c": "conditions",
+    }
+
+    trade_mapping = {
+        **base_mapping,
+        "i": "id",
+        "x": "exchange",
+        "p": "price",
+        "s": "size",
+        "z": "tape",
+    }
+    quote_mapping = {
+        **base_mapping,
+        "ax": "ask_exchange",
+        "ap": "ask_price",
+        "as": "ask_size",
+        "bx": "bid_exchange",
+        "bp": "bid_price",
+        "bs": "bid_size",
+        "z": "tape",
+    }
+    bar_mapping = {
+        **base_mapping,
+        "o": "open",
+        "h": "high",
+        "l": "low",
+        "c": "close",
+        "v": "volume",
+        "n": "trade_count",
+        "vw": "vwap",
+    }
+
+    if snapshot is None:
+        return {}
+
+    trade_data = (
+        {
+            trade_mapping[key]: getattr(snapshot.latest_trade, key)
+            for key in trade_mapping
+            if hasattr(snapshot.latest_trade, key)
+        }
+        if snapshot.latest_trade
+        else {}
+    )
+    quote_data = (
+        {
+            quote_mapping[key]: getattr(snapshot.latest_quote, key)
+            for key in quote_mapping
+            if hasattr(snapshot.latest_quote, key)
+        }
+        if snapshot.latest_quote
+        else {}
+    )
+    minute_bar_data = (
+        {
+            bar_mapping[key]: getattr(snapshot.minute_bar, key)
+            for key in bar_mapping
+            if hasattr(snapshot.minute_bar, key)
+        }
+        if snapshot.minute_bar
+        else {}
+    )
+    daily_bar_data = (
+        {
+            bar_mapping[key]: getattr(snapshot.daily_bar, key)
+            for key in bar_mapping
+            if hasattr(snapshot.daily_bar, key)
+        }
+        if snapshot.daily_bar
+        else {}
+    )
+    prev_daily_bar_data = (
+        {
+            bar_mapping[key]: getattr(snapshot.prev_daily_bar, key)
+            for key in bar_mapping
+            if hasattr(snapshot.prev_daily_bar, key)
+        }
+        if snapshot.prev_daily_bar
+        else {}
+    )
+
+    return {
+        "symbol": symbol,
+        "trade": trade_data,
+        "quote": quote_data,
+        "minute_bar": minute_bar_data,
+        "daily_bar": daily_bar_data,
+        "prev_daily_bar": prev_daily_bar_data,
+    }
+
+
+def fetch_and_process(symbols):
+    """Fetch and process snapshots for given symbols.
+
+    Args:
+        symbols: List of symbols.
+
+    Returns:
+        A pandas DataFrame with the processed data.
+    """
+    snapshots = fetch_snapshots(symbols)
+    data = [
+        process_snapshot(symbol, snapshot)
+        for symbol, snapshot in snapshots.items()
+    ]
+    return pd.json_normalize(data, sep="_")
+
+
+def filter_data(df, data_type):
+    """Filter data by type.
+
+    Args:
+        df: A pandas DataFrame with the data.
+        data_type: The type of data to filter by.
+
+    Returns:
+        A filtered pandas DataFrame.
+    """
+    if data_type != "all":
+        columns_to_keep = ["symbol"] + df.columns[df.columns.str.contains(data_type, case=False)].tolist()
+        df = df[columns_to_keep]
+    if not df.empty and "symbol" in df.columns:
+        df = df[["symbol"] + [col for col in df.columns if col != "symbol"]]
+    return df
+
+
+def get_symbols_from_file(file_path):
+    """Get symbols from a file.
+
+    Args:
+        file_path: The path to the file.
+
+    Returns:
+        A list of symbols, or None if the file does not exist or is empty.
+    """
+    if not os.path.exists(file_path):
+        print(f"File {file_path} does not exist.")
+        return None
+
+    with open(file_path, "r") as file:
+        symbols = [line.strip().upper() for line in file]
+
+    if not symbols:
+        print(f"File {file_path} is empty.")
+        return None
+
+    return symbols
+
+
+def main(args):
+    """Main function to run the script.
+
+    Args:
+        args: Command line arguments.
+    """
+    symbols = None
+    if args.list:
+        if not os.path.exists(args.list):
+            print(f"Stock symbol list file {args.list} does not exist.")
+            return
+        symbols = get_symbols_from_file(args.list)
+
+    if not symbols:
+        symbols = [
+            el.symbol
+            for el in rest_api.list_assets(status="active")
+            if len(el.symbol) < 5
+        ]
+
+    df = fetch_and_process(symbols)
+    df = filter_data(df, args.type)
+
+    # Drop rows with NaN values
+    df.dropna(inplace=True)
+
+    print(df)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-l",
+        "--list",
+        required=False,
+        help="Path to the file containing the stock symbols",
+    )
+    parser.add_argument(
+        "-t",
+        "--type",
+        default="all",
+        choices=[
+            "all",
+            "trade",
+            "quote",
+            "minute_bar",
+            "daily_bar",
+            "prev_daily_bar",
+        ],
+        help="Type of data to output",
+    )
+    args = parser.parse_args()
+    main(args)
